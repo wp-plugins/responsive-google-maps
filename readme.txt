f622863870e136175eb1956b898f910509585a07
Zaglov <Zaglov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-10 07:23:37 +0000

    Deployment of Plug-In to Repository
    git-svn-id: https://plugins.svn.wordpress.org/responsive-google-maps/trunk@1131811 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,30 @@
+Plug-In Description:
+
+The responsive Goolge Maps Plug-In brings Responsive Google Maps to your site.
+When the browser-window is resized, the Plug-In automatically recenters the map view to the desired coordinates. You can activate and deactivate map control elements, set the height and the type of map (road, satellite).
+
+And that’s pretty much it.
+
+Installation:
+
+Just download and install the Plug-In as usual and you are good to go.
+
+Minimalistic example:
+
+[responsive_map lat="51.44303" lng="7.01247"]
+
+Possible Parameters:
+
+lng (float): 51.44303  / Longitude
+lat (float): 7.01247 / Latitude
+style (string) = null / Display normal map Styling or set it to greyscale by setting style to „monochrome“
+height (str) = 400px / Height of the Container of the Map
+zoom (int) = 10 / Zoom Level of the Map
+show_marker (bool) = true / Should a marker be displayed?
+title (str) = null / Title of the Marker
+zoom_control (bool) = true / Enable or disable Zoom-Conrol
+map_type_control (bool) = true / Enable or disable Map-Type-Conrol
+scale_control (bool) = true / Enable or disable Scale Control
+street_view_control (bool) = true / Enable or disable Street View
+overview_map_control (bool) = true / Enable or disable Map Control
+map_type (str) = road / Set Map Typ to Road („road“) or satellite („sattelite“)
---
+++ responsive_google_maps.php
@@ -0,0 +1,107 @@
+<?php
+defined('ABSPATH') or die("No script kiddies please!");
+
+/*
+
+    Plugin Name: Responsive Google Maps
+    Description: This Plug-In displays responsive and configurable Maps by Google Maps in your WordPress Site
+    Version: 1.0.2
+    Author: Ilja Zaglov | imbaa Kreativagentur
+    Author URI: http://www.imbaa.de
+    Network: true
+    License: GPL2
+
+    Copyright 2015  Ilja Zaglov  (email : ilja.zaglov@imbaa.de)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+ */
+
+
+class responsive_maps {
+
+    public function __construct()
+    {
+
+
+        add_shortcode( 'responsive_map', array($this,'responsive_map_func') );
+        add_action('init',array($this,'register_scripts'));
+
+    }
+
+
+    public function register_scripts(){
+
+        wp_register_script( 'googleMaps', 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=visualization' );
+        wp_register_script( 'responsiveMaps', plugins_url('responsive_google_maps/js/responsiveMaps.js' ),array('jquery','googleMaps'),'1.0.2',true);
+
+    }
+
+    public function responsive_map_func($args){
+
+        $args = shortcode_atts( array(
+            'lat' => 50.9847679,
+            'lng' => 11.0298799,
+            'style' => null,
+            'zoom' => 10,
+            'title' => null,
+            'height' => '400px',
+            'zoom_control' => true,
+            'map_type_control' => true,
+            'scale_control' => true,
+            'street_view_control' => true,
+            'overview_map_control' => true,
+            'show_marker' => true,
+            'map_type' => 'road'
+
+        ), $args );
+
+
+
+
+        $id = 'responsiveMap'.md5(time().microtime());
+
+        wp_enqueue_script('responsiveMaps');
+
+        $output = '<div
+                            class="responsiveMap"
+                            id="'.$id.'"
+                            style="height:'.$args['height'].';"
+                            data-title="'.$args['title'].'"
+                            data-lat="'.$args['lat'].'"
+                            data-lng="'.$args['lng'].'"
+                            data-style="'.$args['style'].'"
+                            data-zoom="'.$args['zoom'].'",
+                            data-maptypecontrol="'.$args['map_type_control'].'",
+                            data-scalecontrol="'.$args['scale_control'].'",
+                            data-streetviewcontrol="'.$args['street_view_control'].'",
+                            data-overviewmapcontrol="'.$args['overview_map_control'].'",
+                            data-showmarker="'.$args['show_marker'].'",
+                            data-maptype="'.$args['map_type'].'"
+                ></div>';
+
+        return $output;
+
+    }
+}
+
+
+
+$plugin = new responsive_maps();
+
+
+
+
+?>
