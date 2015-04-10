2e4fe08ef60465c44f2f10a3a3ddf3591675e2cc
Zaglov <Zaglov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-10 08:26:10 +0000

    First Release
    git-svn-id: https://plugins.svn.wordpress.org/responsive-google-maps/tags/1.0.3@1131850 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/responsiveMaps.js
@@ -0,0 +1,150 @@
+/*!
+ * jQuery Responsive Maps Plug-In
+ * version: 1.0.2
+ * Requires jQuery v1.5 or later
+ * Copyright (c) 2015 Ilja Zaglov | imbaa Kreativagentur
+ * Licensed under GPL
+ */
+
+var responsiveMap = function(mapID){
+
+    $ = jQuery;
+
+
+    var element = $('#'+mapID);
+
+
+
+
+
+    var self = this;
+
+    self.map = null;
+
+
+    self.init = function() {
+
+
+        var args = element.data();
+
+
+
+
+
+
+
+        if(args.style == 'monochrome'){
+
+            var styleArray = [
+                {
+                    featureType: "all",
+                    stylers: [
+                        { saturation: -100 }
+                    ]
+                },{
+                    featureType: "road.arterial",
+                    elementType: "geometry",
+                    stylers: [
+
+                        { saturation: 0 }
+                    ]
+                },{
+                    featureType: "poi.business",
+                    elementType: "labels",
+                    stylers: [
+                        { visibility: "off" }
+                    ]
+                }
+            ];
+
+
+        } else {
+
+            var styleArray = [];
+
+        }
+
+
+
+        if(args.maptype == 'satellite'){
+
+            args.maptype = google.maps.MapTypeId.SATELLITE;
+
+        } else if (args.maptype == 'road'){
+
+            args.maptype = google.maps.MapTypeId.ROAD;
+
+        }
+
+
+
+
+        var mapOptions = {
+            zoom: parseInt(args.zoom),
+            center: new google.maps.LatLng(args.lat, args.lng),
+            styles:styleArray,
+            zoomControl: args.zoomcontrol,
+            disableDoubleClickZoom: false,
+            mapTypeControl: args.maptypecontrol,
+            scaleControl:args.scalecontrol,
+            scrollwheel: false,
+            panControl: args.pancontrol,
+            streetViewControl: args.streetviewcontrol,
+            draggable : false,
+            overviewMapControl: args.overviewmapcontrol,
+            overviewMapControlOptions: {
+                opened: args.overviewmapcontrol
+            },
+            mapTypeId: args.maptype
+
+        };
+
+        self.map = new google.maps.Map(document.getElementById(mapID), mapOptions);
+
+
+        if(args.showmarker == true){
+
+            new google.maps.Marker({
+                position: new google.maps.LatLng(args.lat, args.lng),
+                map: self.map,
+                title: element.attr('title')
+            });
+
+        }
+
+
+        $(window).bind('resize',function(){
+
+            self.map.setCenter(new google.maps.LatLng(args.lat, args.lng));
+
+        });
+
+
+
+
+
+    }
+
+    self.init(mapID);
+
+}
+
+
+$ = jQuery;
+
+$(document).ready(function(){
+
+
+    $('.responsiveMap').each(function(){
+
+
+        new responsiveMap($(this).attr('id'));
+
+
+    });
+
+
+});
+
+
+
---
+++ readme.txt
@@ -0,0 +1,51 @@
+=== Plugin Name ===
+Contributors: Zaglov
+Tags: responsive, google, google maps
+Requires at least: 3.0.1
+Tested up to: 4.3
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This Plug-In displays responsive and configurable Maps by Google Maps in your WordPress Site.
+
+== Description ==
+
+The responsive Goolge Maps Plug-In brings Responsive [Google Maps API](https://developers.google.com/maps/?hl=de "Google Maps API") via the Google Maps API  to your site.
+When the browser-window is resized, the Plug-In automatically recenters the map view to the desired coordinates. You can activate and deactivate map control elements, set the height and the type of map (road, satellite).
+
+And that’s pretty much it.
+
+How to use:
+
+The Plug-In requires at leas two values to work: the latitude and the longitude of the location to display
+
+[responsive_map lat="51.44303" lng="7.01247"]
+
+If you need to use the Shortcode inside a theme, just use `<?php do_shortcode('[responsive_map lat="51.44303" lng="7.01247"]') ?>`.
+
+See it in action on [imbaa Kreativagentur](http://www.imbaa.de "imbaa Kreativagentur Essen")'s site.
+
+Possible other Parameters:
+
+
+*   lng: 51.44303  / Longitude
+*   lat: 7.01247 / Latitude
+*   height = 400px / Height of the Container of the Map
+*   zoom = 10 / Zoom Level of the Map
+*   show_marker = true / Should a marker be displayed?
+*   title = null / Title of the Marker
+*   zoom_control = true / Enable or disable Zoom-Conrol
+*   map_type_control = true / Enable or disable Map-Type-Conrol
+*   scale_control = true / Enable or disable Scale Control
+*   street_view_control = true / Enable or disable Street View
+*   overview_map_control = true / Enable or disable Map Control
+*   map_type (str) = road / Set Map Typ to Road („road“) or satellite („sattelite“)
+
+
+== Installation ==
+
+
+1. Upload the Plug-In Direcotry into the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Use the [responsive_map] Shortcode in your Site
